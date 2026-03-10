005657516db461a3d1bce776d0bf722ea54d1cb7
Paul Ancona <paulancona9826@gmail.com>
2026-03-10 11:40:10 -0600

    feat(rive): wire up isChecking/isHandsUP inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,18 +9,107 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //control  para mostrar o ocultar contraseña
+  bool _obscureText = true;
+
+  //cerebro de la logica de la animacion
+  StateMachineController? _controller;
+  //State machine input
+  SMIBool? _isChecking;
+  SMIBool? _isHandsUp;
+  SMINumber? _numLook;
+  SMITrigger? _trigSuccess;
+  SMITrigger? _trigFail;
+
   @override
   Widget build(BuildContext context) {
+    final Size = MediaQuery.of(context).size;
+
     return Scaffold(
       //evita nudge o camaras frontales
-      body: SafeArea(child:
-      Column(
-        children: [
-          Expanded(child: RiveAnimation.asset('animated_login_character.riv'))
-        ],
-      ) 
-      
-      
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 100.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: Size.width,
+                height: Size.height * 0.4,
+                child: RiveAnimation.asset(
+                  'animated_login_character.riv',
+                  stateMachines: ["Login Machine"],
+                  // al iniciansrse la animacion se ejecuta esta funcion
+                  onInit: (artboard) {
+                    _controller = StateMachineController.fromArtboard(
+                      artboard,
+                      'Login Machine',
+                    );
+
+                    //vrificae que inicio bien la animacion
+                    if (_controller == null) return;
+                    artboard.addController(_controller!);
+
+                    //vinculando los input de la animacion con las variables del codigo
+                    _isChecking = _controller!.findSMI('isChecking') as SMIBool;
+                    _isHandsUp = _controller!.findSMI('isHandsUp') as SMIBool;
+                    _numLook = _controller!.findSMI('numLook') as SMINumber;
+                    _trigSuccess =
+                        _controller!.findSMI('trigSuccess') as SMITrigger;
+                    _trigFail = _controller!.findSMI('trigFail') as SMITrigger;
+                  },
+                ),
+              ),
+              const SizedBox(height: 10),
+              //Email
+              TextField(
+                onChanged: (value) {
+                  if (_isHandsUp != null) {
+                    _isHandsUp!.change(false);
+                  }
+                  if (_isChecking == null) return;
+                  _isChecking!.change(true);
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.email),
+                  hintText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              //Password
+              TextField(
+                onChanged: (value) {
+                  if (_isChecking != null) {
+                    _isChecking!.change(false);
+                  }
+                  if (_isHandsUp == null) return;
+                  _isHandsUp!.change(true);
+                },
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: const Icon(Icons.lock),
+                  hintText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
